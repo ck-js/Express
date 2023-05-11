313a11e515108c00a4fccd907e0c740ed06fd0f7
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-05-11 20:20:32 +0700

    Add response objects to send items back to the front end from the server. We use the response with the send, status along with send as well as redirect methods when we want to re direct the root path or url.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.get("/", (request,response) => {
 console.log(request)
@@ -15,6 +15,27 @@ app.get("/flag/:country", (req, res) => {
     console.log(`New request made: ${req.method}`);
     console.log(`Country parameter: ${req.params.country}`)
 });
+
+app.get("/pets/:petName", (req,res) => {
+    console.log(`The pets name is: ${req.params.petName}`);
+})
+
+app.get("/about", (req,res) => {
+    console.log(`New request made: ${req.method}`);
+    res.send("<h1>About</h1>");
+});
+
+app.get("/", (req, res) => {
+    console.log(`New request made: ${req.method}`);
+    res.status(200).send("<p>Successfull Request</p>")
+});
+
+
+app.get("/user", (req, res) => {
+    console.log(`New request made: ${req.method}`);
+    res.redirect("/userinfo");
+    res.send("<h1>User Info</h1>");
+});;
 
 
 
