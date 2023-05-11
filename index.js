be3e575f465eba8d8f715581b4201adc54642c1c
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-05-11 19:06:41 +0700

    Add code example for request objects displaying request methods and parameters in a console log statement wrapped inside of template literals

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello world")
-})
+app.get("/", (request,response) => {
+console.log(request)
+});
+
+app.get("/", (req, res) => {
+    console.log(`New request made: ${req.method}`);
+    console.log(`Hostname: ${req.hostname}`);
+});
+
+app.get("/flag/:country", (req, res) => {
+    console.log(`New request made: ${req.method}`);
+    console.log(`Country parameter: ${req.params.country}`)
+});
+
+
+
+// app.get('/', (req, res) => {
+//     res.send("Hello world, this is backend")
+// })
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
